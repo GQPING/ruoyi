6f18e9eca487fd6727babc008760cda34825a763
chengxianwei <1878776239@qq.com>
2022-02-06 06:56:26 +0800

    config

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -35,7 +35,8 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://101.43.175.187:28080`,
+        target: `http://127.0.0.1:28080`,
+        //target: `http://101.43.175.187:28080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
